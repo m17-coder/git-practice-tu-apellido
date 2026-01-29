7859c2b9f1d3967799ae0cf3ac9ca65fca7a52d2
Pablo Campos <pablocampos0512@gmail.com>
2026-01-28 20:51:10 -0400

    feat: crear programa hola mundo inicial

---
+++ hola_mundo.cpp
@@ -0,0 +1,9 @@
+// hola_mundo.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "¡Hola Mundo desde Git!" << endl;
+    cout << "Mi nombre es: [Tu Nombre]" << endl;
+    return 0;
+}
